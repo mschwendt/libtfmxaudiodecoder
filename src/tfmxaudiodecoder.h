8cf8873e10080b2d3d44edf35a026ec1e8625333
Michael Schwendt <mschwendt@fedoraproject.org>
2025-12-18 22:16:07 +0100

    update, although some more reviews might be coming

--- src/tfmxaudiodecoder.h
+++ src/tfmxaudiodecoder.h
@@ -35,7 +35,7 @@
  * only support filtering/sorting by file name extension.
  */
 
-#define TFMXAUDIODECODER_VERSION 010000
+#define TFMXAUDIODECODER_VERSION 010001
 
 #ifdef __cplusplus
 extern "C" {

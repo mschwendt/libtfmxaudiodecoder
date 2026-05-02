84baa3e66ebb8430ef9eba0401ca7677e1b6da78
Michael Schwendt <mschwendt@fedoraproject.org>
2026-05-02 11:49:36 +0200

    v1.0.6

--- src/tfmxaudiodecoder.h
+++ src/tfmxaudiodecoder.h
@@ -35,7 +35,7 @@
  * only support filtering/sorting by file name extension.
  */
 
-#define TFMXAUDIODECODER_VERSION 010005
+#define TFMXAUDIODECODER_VERSION 010006
 
 #ifdef __cplusplus
 extern "C" {
